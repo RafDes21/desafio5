4eed034656f80e2a9cecab7af4d2362ab122e19e
unknown <israeltineo84@gmail.com>
2023-03-20 13:46:21 -0300

    Tab

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+
+import AppNavigator from "./navigation";
+import { styles } from "./styles";
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
+  const [loaded] = useFonts({
+    "Roboto-Bold": require("../assets/fonts/RobotoCondensed-Bold.ttf"),
+    "Roboto-Light": require("../assets/fonts/RobotoCondensed-Light.ttf"),
+    "Roboto-Regular": require("../assets/fonts/RobotoCondensed-Regular.ttf"),
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  if (!loaded) {
+    return (
+      <View>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return <AppNavigator />;
+};
 
 export default App;
